2e96a8d6d3b7bd88ddef16dc09a8745ca8e611d6
Ben Guo <ben@venmo.com>
2014-04-13 17:42:27 -0400

    1.4, 1.5

---
+++ 1/ex1_3.scm
@@ -0,0 +1,15 @@
+(define (square a) (* a a))
+
+(define (sum-of-squares a b) (+ (square a) (square b)))
+
+(define (max a b) (if (> a b) a b))
+
+(define (sum-of-largest-squares a b c) 
+	(if (> a b) (sum-of-squares a (max b c)) (sum-of-squares b (max a c))))
+
+(newline)
+(display (= (sum-of-largest-squares 1 3 4) 25))
+(newline)
+(display (= (sum-of-largest-squares 4 2 1) 20))
+(newline)
+(display (= (sum-of-largest-squares 4 2 5) 41))
---
+++ 1/ex1_5.scm
@@ -0,0 +1,13 @@
+(define (p) (p))
+(define (test x y) (if (= x 0) 0 y))
+(test 0 (p))
+
+; applicative
+(test 0 (p))
+(test 0 (p))
+etc.
+
+; normal
+(test 0 (p))
+(if (= 0 0) 0 p)
+0
